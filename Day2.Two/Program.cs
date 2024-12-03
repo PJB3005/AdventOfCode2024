bea40d1b91d8bf746117b71b2b6a6c9d8a917d91
PJB3005 <pieterjan.briers+git@gmail.com>
2024-12-03 18:22:07 +0100

    Day 2

---
+++ Day2.Two/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+using Day2;
+
+var input = Input.Text;
+var reader = new StringReader(input);
+
+var list = new List<int[]>();
+while (reader.ReadLine() is { } line)
+{
+    list.Add(line.Trim().Split(' ').Select(int.Parse).ToArray());
+}
+
+Console.WriteLine(list.Where(IsReportSafeForReal).Count());
+
+return;
+
+bool IsReportSafeForReal(int[] report)
+{
+    if (IsReportSafe(report))
+        return true;
+
+    for (var i = 0; i < report.Length; i++)
+    {
+        var reportList = report.ToList();
+        reportList.RemoveAt(i);
+        if (IsReportSafe(CollectionsMarshal.AsSpan(reportList)))
+            return true;
+    }
+
+    return false;
+}
+
+bool IsReportSafe(ReadOnlySpan<int> report)
+{
+    var sign = Math.Sign(report[0] - report[1]);
+
+    var last = report[0];
+    for (var i = 1; i < report.Length; i++)
+    {
+        var next = report[i];
+
+        if (sign != Math.Sign(last - next))
+            return false;
+
+        var diff = Math.Abs(last - next);
+        if (diff is not (1 or 2 or 3))
+            return false;
+
+        last = next;
+    }
+
+    return true;
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Day2;
+
+var input = Input.Text;
+var reader = new StringReader(input);
+
+var list = new List<int[]>();
+while (reader.ReadLine() is { } line)
+{
+    list.Add(line.Trim().Split(' ').Select(int.Parse).ToArray());
+}
+
+foreach (var report in list.Where(IsReportSafe))
+{
+    Console.WriteLine(string.Join(" ", report.Select(x => x.ToString())));
+}
+
+Console.WriteLine(list.Where(IsReportSafe).Count());
+
+return;
+
+bool IsReportSafe(int[] report)
+{
+    var sign = Math.Sign(report[0] - report[1]);
+
+    var last = report[0];
+    for (var i = 1; i < report.Length; i++)
+    {
+        var next = report[i];
+
+        if (sign != Math.Sign(last - next))
+            return false;
+
+        var diff = Math.Abs(last - next);
+        if (diff is not (1 or 2 or 3))
+            return false;
+
+        last = next;
+    }
+
+    return true;
+}
+
+
