a10c150555b3f94f31b36e41b63ef7837b499ec4
PJB3005 <pieterjan.briers+git@gmail.com>
2024-12-03 17:59:20 +0100

    Day 1

---
+++ Day1.Two/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Day1;
+
+var input = Input.Text();
+
+var regex = new Regex(@"^\s*(\d+)\s+(\d+)\s*$", RegexOptions.Multiline);
+
+var listA = new List<int>();
+var listB = new List<int>();
+
+foreach (Match match in regex.Matches(input))
+{
+    var a = int.Parse(match.Groups[1].Value);
+    var b = int.Parse(match.Groups[2].Value);
+
+    listA.Add(a);
+    listB.Add(b);
+}
+
+var counts = new Dictionary<int, int>();
+foreach (var i in listB)
+{
+    ref var entry = ref CollectionsMarshal.GetValueRefOrAddDefault(counts, i, out _);
+
+    entry += 1;
+}
+
+var totalDistance = listA.Sum(a => a * counts.GetValueOrDefault(a));
+
+Console.WriteLine(totalDistance);
---
+++ Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using Day1;
+
+var input = Input.Text();
+
+var regex = new Regex(@"^\s*(\d+)\s+(\d+)\s*$", RegexOptions.Multiline);
+
+var listA = new List<int>();
+var listB = new List<int>();
+
+foreach (Match match in regex.Matches(input))
+{
+    var a = int.Parse(match.Groups[1].Value);
+    var b = int.Parse(match.Groups[2].Value);
+
+    listA.Add(a);
+    listB.Add(b);
+}
+
+listA.Sort();
+listB.Sort();
+
+long totalDistance = 0;
+for (var i = 0; i < listA.Count; i++)
+{
+    var a = listA[i];
+    var b = listB[i];
+
+    var distance = Math.Abs(a - b);
+
+    Console.WriteLine($"{a} dist {b} = {distance}");
+
+    totalDistance += distance;
+}
+
+Console.WriteLine(totalDistance);
