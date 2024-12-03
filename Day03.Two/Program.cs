c752bee306f27aa48b4cb403e8b3143b5542ac14
PJB3005 <pieterjan.briers+git@gmail.com>
2024-12-03 19:24:24 +0100

    Day 3

---
+++ Day03.Two/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using Day03;
+
+var input = Input.Text;
+var regex = new Regex(@"(?:mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))");
+
+// This is horrible abuse of LINQ, but it's not like I'm the first person to commit it,
+// so it's not like Microsoft can ever break it!
+var enabled = true;
+
+Console.WriteLine(regex.Matches(input).Select(match =>
+{
+    if (match.Value == "do()")
+    {
+        Console.WriteLine("do()");
+        enabled = true;
+        return 0;
+    }
+
+    if (match.Value == "don't()")
+    {
+        Console.WriteLine("don't()");
+        enabled = false;
+        return 0;
+    }
+
+    if (!enabled)
+        return 0;
+
+    Console.WriteLine(match.Value);
+    var a = int.Parse(match.Groups[1].Value);
+    var b = int.Parse(match.Groups[2].Value);
+    return (long)(a * b);
+}).Sum());
---
+++ Day03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using Day03;
+
+var input = Input.Text;
+var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+Console.WriteLine(regex.Matches(input).Select(match =>
+{
+    Console.WriteLine(match.Value);
+    var a = int.Parse(match.Groups[1].Value);
+    var b = int.Parse(match.Groups[2].Value);
+    return (long)(a * b);
+}).Sum());
